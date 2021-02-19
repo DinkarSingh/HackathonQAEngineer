8c9adcffc9af71c03a3a9e42b66a0b87513a33ee
Dinkar Singh K <d.karanvanshi@jellysmack.com>
2021-02-19 11:10:05 +0100

    data file added

--- test/specs/Core/apiTest.spec.ts
+++ test/specs/Core/apiTest.spec.ts
@@ -1,10 +1,12 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import { HttpHeaders } from "@angular/common/http";
+import path from 'path';
 chai.use(chaiHttp);
 chai.should();
 
 describe("POST /api/tasks", () => {
+    let userId = "";
     it("Registerthe user", (done) => {
         const randomNum = Math.floor((Math.random() * 1000) + 1);
         const lastName = 'Singh' + randomNum;
@@ -29,12 +31,37 @@ describe("POST /api/tasks", () => {
             .end((err, response) => {
                 console.log(response)
                 response.should.have.status(200);
-                const ss = response.body.customerId;
-                console.log(ss);
+                const id = response.body.customerId;
+                userId = id;
+                console.log(userId);
                 // response.body.should.be.a('object');
                 // response.body.should.have.property('id').eq(4);
                 // response.body.should.have.property('name').eq("Task 4");
                 // response.body.should.have.property('completed').eq(false);
+                done();
+            });
+    });
+
+    it("Send the user receipt", (done) => {
+        const filePath = path.join(__dirname, '/../../../data/What.png');
+        console.log(userId);
+        const obj = {
+            "customerId": userId,
+            "image": filePath
+        }
+        var url = 'https://api-test.transactionconnect.com';
+        chai.request(url)
+            .post("/auth/receipt")
+            .set('authority', 'api-test.transactionconnect.com')
+            .set('Accept', 'application/json, text/plain, */*')
+            .set('content-type', 'multipart/form-data; boundary=----WebKitFormBoundaryxJ3kJmlkyWxlRquZ')
+            .set('origin', 'https://tcmall-test.transactionconnect.com')
+            .set('referer', 'https://tcmall-test.transactionconnect.com/')
+            .set('accept-language', 'en-GB,en-US;q=0.9,en;q=0.8')
+            .send(obj)
+            .end((err, response) => {
+                console.log(response);
+                response.should.have.status(200);
                 done();
             });
     });
